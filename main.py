e29f1a92abf7cf9561159c5a779e07ed19d6fc6e
batuhanbat <64826454+batuhanbat@users.noreply.github.com>
2022-12-07 22:27:22 +0300

    updated main

--- main.py
+++ main.py
@@ -3,17 +3,13 @@ from sklearn import linear_model
 
 df = pandas.read_csv("data.csv")
 
-independent_values = df[['Weight', 'Volume']]
-dependent_values = df['CO2']
+independent_values = df[['TUFE', 'USD', 'EUR', 'GBP', 'CHF', 'ALTIN']]
+dependent_values = df['TURKIYE']
 
 regr = linear_model.LinearRegression()
 regr.fit(independent_values.values, dependent_values.values)
 
-# predict the CO2 emission of a car where the weight is 2300kg, and the volume is 1300cm3:
-predictedCO2 = regr.predict([[2300, 1300]])
+# predict the Turkiye konut price(TL/m2) where TUFE=1500, USD=27, EUR=28, GBP=30, CHF=29, ALTIN=1600
+predictedTurkiye = regr.predict([[1500, 27, 28, 30, 29, 1600]])
 
-print(predictedCO2)
-print(regr.coef_)
-
-# regr.predict([[3300, 1300]]) diye cagırınca [114.75968007] donuyor
-# 107.2087328 + (1000 * 0.00755095) = 114.75968
+print(predictedTurkiye)
